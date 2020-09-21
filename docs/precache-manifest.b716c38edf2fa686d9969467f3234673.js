a578fca65a7e0be35403a4b60cfe587644064b01
Kayla Jean Crane <kayla.crane@gmail.com>
2020-09-21 13:15:53 +0200

    Update docs

---
+++ docs/precache-manifest.b716c38edf2fa686d9969467f3234673.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792f9fecb9969eaf12512dc5b09686d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "38d3c778a67423005103",
+    "url": "./static/css/main.25c3e660.chunk.css"
+  },
+  {
+    "revision": "27082abf509cb9ded339",
+    "url": "./static/js/2.d6b06c9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d6b06c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d3c778a67423005103",
+    "url": "./static/js/main.7d1a6fd3.chunk.js"
+  },
+  {
+    "revision": "e5a353c430b206cc5b3a",
+    "url": "./static/js/runtime-main.45ecce0e.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "./static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "45feccda85a84d47f989b238df8087a8",
+    "url": "./static/media/universe.45feccda.jpg"
+  }
+]);
